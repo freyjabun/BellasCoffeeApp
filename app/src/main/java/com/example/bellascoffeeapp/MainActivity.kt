fd760e89ee277be189f4dec70b9d505ccc6e667b
Freyja <freyjabun@outlook.dk>
2025-02-17 13:19:12 +0100

    feat: added ShopItem composable

--- app/src/main/java/com/example/bellascoffeeapp/MainActivity.kt
+++ app/src/main/java/com/example/bellascoffeeapp/MainActivity.kt
@@ -13,18 +13,23 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -32,16 +37,34 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.bellascoffeeapp.ui.theme.BellasCoffeeAppTheme
+import com.example.bellascoffeeapp.ui.theme.BellasTheme
 
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            BellasCoffeeAppTheme {
-                Greeting()
+            BellasTheme {
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            colors = topAppBarColors(
+                                containerColor = Color(0xFFCCC2DC),
+                                titleContentColor = Color.Black,
+                            ),
+                            title = {
+                                Text("Bella's Coffee Lab")
+                            }
+                        )
+                    },
+                ) { innerPadding ->
+                    Column (modifier = Modifier
+                        .padding(innerPadding)){
+                        Greeting()
+                    }
+                }
                 }
             }
         }
@@ -50,7 +73,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(){
-    var name by remember { mutableStateOf("") }
+    var name by rememberSaveable { mutableStateOf("") }
     var enteredName by rememberSaveable { mutableStateOf("") }
     var introVisible by rememberSaveable { mutableStateOf( true) }
     var welcomeVisible by rememberSaveable { mutableStateOf(false) }
@@ -75,38 +98,7 @@ fun Greeting(){
                 text = "What is your name?",
                 fontSize = 20.sp
             )
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(
-                    10.dp,
-                    alignment = Alignment.CenterHorizontally
-                )
-            ) {
-                OutlinedTextField(
-                    value = name,
-                    onValueChange = { text ->
-                        name = text
-                    }
-                )
-                Button(
-                    onClick = {
-                        if (name.isNotBlank()) {
-                            enteredName = name
-                            introVisible = !introVisible
-                            welcomeVisible = !welcomeVisible
-                        }
-                    },
-                    shape = CircleShape,
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.DarkGray)
-                )
-                {
-                    Icon(
-                        imageVector = Icons.Default.Check,
-                        contentDescription = "Add"
-                    )
-                }
-            }
+            TextInput()
         }
     }
     AnimatedVisibility(visible = welcomeVisible,
@@ -124,4 +116,38 @@ fun Greeting(){
     }
 }
 
+@Composable
+fun TextInput(){
+    var name by rememberSaveable { mutableStateOf("") }
+    var enteredName by rememberSaveable { mutableStateOf("") }
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(
+            10.dp,
+            alignment = Alignment.CenterHorizontally
+        )
+    ) {
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it
+            }
+        )
+        Button(
+            onClick = {
+                if (name.isNotBlank()) {
+                    enteredName = name
+                }
+            },
+            shape = CircleShape,
+            colors = ButtonDefaults.buttonColors(containerColor = Color.DarkGray)
+        )
+        {
+            Icon(
+                imageVector = Icons.Default.Check,
+                contentDescription = "Add"
+            )
+        }
+    }
+}
 
---
+++ app/src/main/java/com/example/bellascoffeeapp/ui/theme/BellasFont.kt
@@ -0,0 +1,12 @@
+package com.example.bellascoffeeapp.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.bellascoffeeapp.R
+
+val Helvetica = FontFamily(
+    Font(R.font.helvetica, FontWeight.Normal),
+    Font(R.font.helvetica_bold, FontWeight.Bold),
+    Font(R.font.helvetica_light, FontWeight.Light)
+)
