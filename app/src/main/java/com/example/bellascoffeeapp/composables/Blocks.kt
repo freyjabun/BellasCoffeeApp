42e5362b6ed1157241c20c880325dff3ed13ac7c
Freyja <freyjabun@outlook.dk>
2025-02-17 15:10:26 +0100

    feat: added coil dependency and implemented lazyrow

--- app/src/main/java/com/example/bellascoffeeapp/composables/Blocks.kt
+++ app/src/main/java/com/example/bellascoffeeapp/composables/Blocks.kt
@@ -3,7 +3,6 @@ package com.example.bellascoffeeapp.composables
 
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -31,13 +30,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
-import coil3.request.crossfade
 import com.example.bellascoffeeapp.R
 import com.example.bellascoffeeapp.ui.theme.BellasTheme
 
