ac7d179cf049ac597f5db9d52dc3663e0aff9a5a
Freyja <freyjabun@outlook.dk>
2025-05-23 16:55:00 +0200

    Set up compose structure for details screen

--- app/src/main/java/com/example/bellascoffeeapp/orders/view/OrderDetailsView.kt
+++ app/src/main/java/com/example/bellascoffeeapp/orders/view/OrderDetailsView.kt
@@ -5,9 +5,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -23,6 +24,7 @@ import coil3.compose.AsyncImage
 import coil3.request.ImageRequest
 import com.example.bellascoffeeapp.R
 import com.example.bellascoffeeapp.orders.model.Drink
+import com.example.bellascoffeeapp.orders.model.ExtraDto
 import com.example.bellascoffeeapp.orders.viewmodel.OrderDetailsViewModel
 import com.example.bellascoffeeapp.ui.theme.BellasTheme
 import kotlinx.serialization.Serializable
@@ -39,8 +41,9 @@ fun OrderDetailsView(viewModel: OrderDetailsViewModel){
 
     Column (Modifier
         .fillMaxSize()
-        .background(BellasTheme.colorScheme.background),
-
+        .background(BellasTheme.colorScheme.background)
+        .padding(5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ){
         if (drink.isEmpty())
         {
@@ -48,15 +51,18 @@ fun OrderDetailsView(viewModel: OrderDetailsViewModel){
         }
         else {
             DrinkItem(drink[0])
+            if(drink[0].type == "coffee"){
+                Extras(extras)
+            }
         }
     }
 }
 
 @Composable
 private fun DrinkItem(drink: Drink){
-
-    Column (horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(10.dp)){
         AsyncImage(
             modifier = Modifier.height(300.dp)
                 .clip(RoundedCornerShape(10.dp)),
@@ -67,12 +73,32 @@ private fun DrinkItem(drink: Drink){
             fallback = painterResource(R.drawable.bellascoffeelab),
             contentDescription = "Image of ${drink.name}"
         )
-        Text(drink.name)
-        Text("")
-        HorizontalDivider(modifier = Modifier.height(2.dp))
+        Text(drink.name, style = BellasTheme.typography.labelLarge)
+        DrinkPrices(drink.sPrice, drink.mPrice, drink.lPrice)
     }
-
 }
+
+@Composable
+private fun DrinkPrices(small: Float?, medium: Float?, large: Float?){
+    Text("Small: $small DKK")
+    Text("Medium: $medium DKK")
+    Text("Large: $large DKK")
+}
+
+@Composable
+private fun Extras(extras: List<ExtraDto>){
+    Text("Extras Here")
+    Column { extras.forEach{
+        ExtrasItem(it)
+        }
+    }
+}
+
+@Composable
+private fun ExtrasItem(extra: ExtraDto){
+    Card {  }
+}
+
 
 @Serializable
 data class OrderDetailsRoute(
